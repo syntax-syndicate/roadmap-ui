8a867ee0750d01812bfface1636012aada9e43ee
Hayden Bleasel <hello@haydenbleasel.com>
2024-08-25 00:27:40 -0400

    Merge tailwind configs

--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -1,13 +1,1 @@
-import { createPreset } from 'fumadocs-ui/tailwind-plugin';
-
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './content/**/*.{md,mdx}',
-    './mdx-components.{ts,tsx}',
-    './node_modules/fumadocs-ui/dist/**/*.js',
-  ],
-  presets: [createPreset()],
-};
+export { default } from '@repo/tailwind-config';
