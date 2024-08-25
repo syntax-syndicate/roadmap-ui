b086dffd90a622addd4fcdb2ca8f96a795f0c434
Hayden Bleasel <hello@haydenbleasel.com>
2024-08-25 10:49:22 -0400

    Update page.tsx

--- apps/docs/app/(home)/page.tsx
+++ apps/docs/app/(home)/page.tsx
@@ -65,12 +65,12 @@ export default function HomePage() {
             target="_blank"
             rel="noreferrer"
             className="relative !py-0 group inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2"
-            href="https://github.com/guilhermerodz/input-otp"
+            href="https://github.com/haydenbleasel/roadmap-ui"
           >
-            <div className="flex items-center h-full">
+            <div className="flex items-center h-full gap-2">
               <Image src={GithubLogo} alt="Github" width={16} height={16} />
-              <div className="hidden md:[display:unset]">Github</div>
-              <div className="hidden md:[display:unset] h-full w-px bg-input group-hover:bg-foreground mx-4" />
+              <div className="hidden md:[display:unset]">GitHub</div>
+              <div className="hidden md:[display:unset] h-full w-px bg-input group-hover:bg-foreground mx-2" />
               <div>2.4K</div>
             </div>
           </a>
