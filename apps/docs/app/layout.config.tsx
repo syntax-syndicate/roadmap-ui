fdf199e4ab8ec7230381a5971f6f456966fa8653
Hayden Bleasel <hello@haydenbleasel.com>
2024-08-25 10:43:38 -0400

    Misc fixes

--- apps/docs/app/layout.config.tsx
+++ apps/docs/app/layout.config.tsx
@@ -1,6 +1,6 @@
-import { type DocsLayoutProps } from 'fumadocs-ui/layout';
-import { type HomeLayoutProps } from 'fumadocs-ui/home-layout';
 import { pageTree } from '@/app/source';
+import type { HomeLayoutProps } from 'fumadocs-ui/home-layout';
+import type { DocsLayoutProps } from 'fumadocs-ui/layout';
 
 // shared configuration
 export const baseOptions: HomeLayoutProps = {
--- apps/docs/tailwind.config.ts
+++ apps/docs/tailwind.config.ts
@@ -3,5 +3,6 @@ import { createPreset } from 'fumadocs-ui/tailwind-plugin';
 
 export default {
   ...config,
+  content: [...config.content, './node_modules/fumadocs-ui/dist/**/*.js'],
   plugins: [...config.plugins, createPreset()],
 };
