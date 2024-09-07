fd8f75ef104e6b8a9b3cf2f93769300e679f85e3
Hayden Bleasel <hello@haydenbleasel.com>
2024-09-06 21:17:20 -0500

    Fix deps

--- apps/docs/tailwind.config.ts
+++ apps/docs/tailwind.config.ts
@@ -1,8 +1,10 @@
 import config from '@repo/tailwind-config';
 import { createPreset } from 'fumadocs-ui/tailwind-plugin';
 
+const preset = createPreset();
+
 export default {
   ...config,
   content: [...config.content, './node_modules/fumadocs-ui/dist/**/*.js'],
-  plugins: [...config.plugins, createPreset()],
+  plugins: [...config.plugins, preset],
 };
