c496bd65e249ef531765ce027245b1c370a63aaa
Hayden Bleasel <hello@haydenbleasel.com>
2024-09-06 21:19:53 -0500

    Fix styles, add lucide dep

--- apps/docs/app/docs/layout.tsx
+++ apps/docs/app/docs/layout.tsx
@@ -1,7 +1,8 @@
+import { source } from '@/app/source';
 import { DocsLayout } from 'fumadocs-ui/layout';
 import type { ReactNode } from 'react';
 import { baseOptions } from '../layout.config';
-import { source } from '@/app/source';
+import 'fumadocs-ui/style.css';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
