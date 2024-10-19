89f7cf103862507f18d2617e7b415292342af379
Hayden Bleasel <hello@haydenbleasel.com>
2024-10-19 19:53:17 -0400

    Fix Gantt docs

--- apps/docs/app/components/gantt.tsx
+++ apps/docs/app/components/gantt.tsx
@@ -12,12 +12,7 @@ import {
   ContextMenuItem,
   ContextMenuTrigger,
 } from '@roadmap-ui/shadcn-ui/components/ui/context-menu';
-import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from '@roadmap-ui/shadcn-ui/components/ui/hover-card';
-import { Separator } from '@roadmap-ui/shadcn-ui/components/ui/separator';
+import {} from '@roadmap-ui/shadcn-ui/components/ui/hover-card';
 // import { toast } from 'sonner';
 import type { Feature } from '@roadmap-ui/types';
 import { EyeIcon, LinkIcon, TrashIcon } from 'lucide-react';
@@ -185,89 +180,56 @@ export const GanttExampleCustom: FC = () => {
           {Object.entries(sortedGroupedFeatures).map(([group, features]) => (
             <Gantt.FeatureListGroup key={group}>
               {features.map((feature) => (
-                <HoverCard key={feature.id}>
-                  <HoverCardTrigger asChild>
-                    <div>
-                      <ContextMenu>
-                        <ContextMenuTrigger asChild>
-                          <button
-                            type="button"
-                            onClick={() => handleViewFeature(feature.id)}
-                          >
-                            <Gantt.FeatureItem
-                              key={feature.id}
-                              onMove={handleMoveFeature}
-                              {...feature}
-                            >
-                              <p className="flex-1 truncate text-xs">
-                                {feature.name}
-                              </p>
-                              {feature.owner && (
-                                <Avatar className="h-4 w-4">
-                                  <AvatarImage src={feature.owner.image} />
-                                  <AvatarFallback>
-                                    {feature.owner.name?.slice(0, 2)}
-                                  </AvatarFallback>
-                                </Avatar>
-                              )}
-                            </Gantt.FeatureItem>
-                          </button>
-                        </ContextMenuTrigger>
-                        <ContextMenuContent>
-                          <ContextMenuItem
-                            className="flex items-center gap-2"
-                            onClick={() => handleViewFeature(feature.id)}
-                          >
-                            <EyeIcon
-                              size={16}
-                              className="text-muted-foreground"
-                            />
-                            View feature
-                          </ContextMenuItem>
-                          <ContextMenuItem
-                            className="flex items-center gap-2"
-                            onClick={() => handleCopyLink(feature.id)}
-                          >
-                            <LinkIcon
-                              size={16}
-                              className="text-muted-foreground"
-                            />
-                            Copy link
-                          </ContextMenuItem>
-                          <ContextMenuItem
-                            className="flex items-center gap-2 text-destructive"
-                            onClick={() => handleRemoveFeature(feature.id)}
-                          >
-                            <TrashIcon size={16} />
-                            Remove from roadmap
-                          </ContextMenuItem>
-                        </ContextMenuContent>
-                      </ContextMenu>
-                    </div>
-                  </HoverCardTrigger>
-                  <HoverCardContent align="start" className="space-y-1">
-                    <p className="font-medium text-xs">{feature.name}</p>
-                    <div className="flex items-center gap-2">
-                      <div
-                        className="h-2 w-2 rounded-full"
-                        style={{ backgroundColor: feature.status.color }}
-                      />
-                      <p className="text-xs">{feature.status.name}</p>
-                    </div>
-                    <Separator />
-                    {feature.owner && (
-                      <div className="flex items-center gap-2">
-                        <Avatar>
-                          <AvatarImage src={feature.owner.image} />
-                          <AvatarFallback>
-                            {feature.owner.name?.slice(0, 2)}
-                          </AvatarFallback>
-                        </Avatar>
-                        <p className="text-xs">{feature.owner.name}</p>
-                      </div>
-                    )}
-                  </HoverCardContent>
-                </HoverCard>
+                <div className="flex" key={feature.id}>
+                  <ContextMenu>
+                    <ContextMenuTrigger asChild>
+                      <button
+                        type="button"
+                        onClick={() => handleViewFeature(feature.id)}
+                      >
+                        <Gantt.FeatureItem
+                          onMove={handleMoveFeature}
+                          {...feature}
+                        >
+                          <p className="flex-1 truncate text-xs">
+                            {feature.name}
+                          </p>
+                          {feature.owner && (
+                            <Avatar className="h-4 w-4">
+                              <AvatarImage src={feature.owner.image} />
+                              <AvatarFallback>
+                                {feature.owner.name?.slice(0, 2)}
+                              </AvatarFallback>
+                            </Avatar>
+                          )}
+                        </Gantt.FeatureItem>
+                      </button>
+                    </ContextMenuTrigger>
+                    <ContextMenuContent>
+                      <ContextMenuItem
+                        className="flex items-center gap-2"
+                        onClick={() => handleViewFeature(feature.id)}
+                      >
+                        <EyeIcon size={16} className="text-muted-foreground" />
+                        View feature
+                      </ContextMenuItem>
+                      <ContextMenuItem
+                        className="flex items-center gap-2"
+                        onClick={() => handleCopyLink(feature.id)}
+                      >
+                        <LinkIcon size={16} className="text-muted-foreground" />
+                        Copy link
+                      </ContextMenuItem>
+                      <ContextMenuItem
+                        className="flex items-center gap-2 text-destructive"
+                        onClick={() => handleRemoveFeature(feature.id)}
+                      >
+                        <TrashIcon size={16} />
+                        Remove from roadmap
+                      </ContextMenuItem>
+                    </ContextMenuContent>
+                  </ContextMenu>
+                </div>
               ))}
             </Gantt.FeatureListGroup>
           ))}
